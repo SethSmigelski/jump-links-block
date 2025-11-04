be525e7a0ae8c0e27c005b940f3d183f89e471ae
Seth Smigelski <51036602+SethSmigelski@users.noreply.github.com>
2025-11-04 13:15:19 -0800

    Update plugin name and URI for SEO 44 version
    Change name for consistent branding with SEO 44 toolkit

--- jump-links-block.php
+++ jump-links-block.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Plugin Name:       Jump Links Block
- * Plugin URI:        https://www.sethcreates.com/plugins-for-wordpress/seo-jump-links-block/
+ * Plugin Name:       Jump Links Block (SEO 44)
+ * Plugin URI:        https://www.sethcreates.com/plugins-for-wordpress/seo-jump-links-block-seo-44/
  * Description:       A standalone custom block for automatically creating a customizable Jump Links table of contents.
  * Version:           1.5
  * Author:            Seth Smigelski
  * Author URI:  	    https://www.sethcreates.com/plugins-for-wordpress/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       jump-links-block
+ * Text Domain:       jump-links-block-seo-44
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
